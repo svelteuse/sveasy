ca94a93776d6d7be63229d6aad5d7239cec81403
Alexander Niebuhr <tech@nbhr.io>
2022-01-11 08:54:52 +0100

    feat: :sparkles: support `CF_PAGES` env variables

--- src/utils/serve.ts
+++ src/utils/serve.ts
@@ -29,7 +29,7 @@ export default async (options: { port?: string }): Promise<void> => {
 
   const define: Record<string, string> = {}
   for (const k in process.env) {
-    if (!k.startsWith('SVEASY_BUILD_VAR_')) continue
+    if (!k.startsWith('SVEASY_BUILD_VAR_') || !k.startsWith('CF_PAGES_')) continue
     define[`process.env.${k}`] = JSON.stringify(process.env[k])
   }
 
